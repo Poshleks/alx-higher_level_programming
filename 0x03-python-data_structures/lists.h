fcc6be43c5cb27b423eb3aeef127fad5ac6b4ba6
Poshleks <adetunjahad@gmail.com>
2023-01-09 17:11:20 +0000

    4-new_in_list.py

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -19,4 +19,14 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
 void def print_reversed_list_integer(my_list=[]):
+void def new_in_list(my_list, idx, element)
+void def no_c(my_string):
+void def print_matrix_integer(matrix=[[]]):
+void def add_tuple(tuple_a=(), tuple_b=()):
+void def multiple_returns(sentence):
+void def max_integer(my_list=[]):
+void def divisible_by_2(my_list=[]):
+void def delete_at(my_list=[], idx=0):
+int is_palindrome(listint_t **head);
+void print_python_list_info(PyObject *p);
 #endif /* LISTS_H */
