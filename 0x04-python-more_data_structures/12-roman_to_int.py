ecb2a0c4d15b639a2366e6fe80c93f14363ebdfd
Poshleks <adetunjahad@gmail.com>
2023-01-11 17:33:44 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,45 +1,67 @@
 #!/usr/bin/python3
+# Check Examples
+
+# python 3.7.1
+
+thousands = ["MMM", "MM", "M"]
+hundreds = ["CM", "DCCC", "DCC", "DC", "D",
+            "CD", "CCC", "CC", "C"]
+tens = ["XC", "LXXX", "LXX", "LX", "L",
+        "XL", "XXX", "XX", "X"]
+digits = ["IX", "VIII", "VII", "VI", "V",
+          "IV", "III", "II", "I"]
+
+
+def check_digit(string, nums):
+    for i in nums:
+        if i in string:
+            return list(reversed(nums)).index(i) + 1
+    return -1
+
+
+def convert_roman_to_int(string):
+    thousand = check_digit(string, thousands)
+    if thousand != -1:
+        delete = thousands[thousand - 1]
+        string = string.replace(delete, '')
+    else:
+        thousand = 0
+    hundred = check_digit(string, hundreds)
+    if hundred != -1:
+        delete = hundreds[hundred - 1]
+        string = string.replace(delete, '')
+    else:
+        hundred = 0
+    ten = check_digit(string, tens)
+    if ten != -1:
+        delete = tens[ten - 1]
+        string = string.replace(delete, '')
+    else:
+        ten = 0
+    digit = check_digit(string, digits)
+    if digit != -1:
+        delete = digits[digit - 1]
+        string = string.replace(delete, '')
+    else:
+        digit = 0
+    return thousand * 1000 + hundred * 100 + ten * 10 + digit
+
+
+def check_input(string):
+    for i in string:
+        if i not in 'MCDLXVI':
+            return False
+    return True
+
+
 def roman_to_int(roman_string):
-    # Fail checks, none, not a string
-    if not roman_string:
+    if roman_string is None:
         return 0
     if not isinstance(roman_string, str):
         return 0
-    if not roman_string.isupper():
+    if not check_input(roman_string):
         return 0
-    # Dictionary for roman numerals
-    r_dict = {
-        "I": 1,
-        "IV": 4,
-        "V": 5,
-        "IX": 9,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
-
-    result = 0
-    temp = list(roman_string)
-    # Concat 4 and 9s
-    if len(temp) > 1:
-        idx = 0
-        for i in temp:
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'V':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'X':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            idx += 1
-    # Search in dict for correct numbers and add
-    for k, v in r_dict.items():
-        for index in temp:
-            if index == k:
-                result += v
+    result = convert_roman_to_int(roman_string)
     return result
+
+  
