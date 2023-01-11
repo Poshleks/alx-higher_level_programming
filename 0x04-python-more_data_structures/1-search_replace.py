a1818a99c636375410e4fe6c05d8c305fc10e074
Poshleks <adetunjahad@gmail.com>
2023-01-11 17:48:45 +0000

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [find if find != search else replace for find in my_list]
-
+    return [replace if num == search else num for num in my_list]
