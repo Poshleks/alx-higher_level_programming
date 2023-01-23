8edffb2ea8856ec4c0166027dc5870c810534016
Poshleks <adetunjahad@gmail.com>
2023-01-23 15:26:20 +0000

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
-
-
-
-if __name__ == "__main__":
-
-    for content in dir(hidden_4):
-
-        if content[:2] != "__":
-
-            print("{:s}".format(content))
+if __name__ = "__main__":
+    import sys
+    import hidden_4
+    for n in dir(hidden_4):
+        if n[:2] != "__":
+            print(n)
