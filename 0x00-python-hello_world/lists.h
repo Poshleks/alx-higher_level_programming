ce0086e069872daf50f4269b594cdb9f33d92512
Poshleks <adetunjahad@gmail.com>
2023-01-23 15:52:49 +0000

    1-run_inline

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -8,13 +8,12 @@
  * @n: integer
  * @next: points to the next node
  *
- * Description: singly linked list node structure
- * for Holberton project
+ * Description: Check singly linked list node structure
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
