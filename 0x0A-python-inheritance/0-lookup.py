49ed32434865c54cb5d405cbeb4da44e2b501d63
Poshleks <adetunjahad@gmail.com>
2023-02-08 21:40:42 +0000

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-lookup.py
+""" Defines an object attributed to LookUp """
+
+
+def lookup(obj):
+    """ function that returns the list of available
+    attributes and methods of an object """
+    return (dir(obj))
