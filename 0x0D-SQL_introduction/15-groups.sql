48c324745ef72d6caf23ffd4f3b43666dee4d9aa
Poshleks <adetunjahad@gmail.com>
2023-03-16 10:39:03 +0000

    15-groups.sql

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+cript that lists the number of records with the same score
+-- Query to lists the number of records with the same score
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY score DESC;
